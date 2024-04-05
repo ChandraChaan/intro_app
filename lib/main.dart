16a0836ee4f975ab0a4b90d26f77e43cc3e845ad
chandraobulareddy <dchandrachaan@gmail.com>
2024-04-05 09:22:11 +0530

    class 3 flutter
    Column,
    Row,
    Container,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-main() {
-  runApp(const MaterialApp(
-    home: Text('Hello World!'),
-  ));
+void main() {
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Hello World!'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Hello World!'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Hello World!'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Hello World!'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Hello World!'),
+                ),
+              ],
+            ),
+            Container(
+              height:50, width:150,child: Text('Hello World!'), color: Colors.amberAccent,),
+            Text('Hello World!'),
+            Text('Hello World!'),
+            Text('Hello World!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
